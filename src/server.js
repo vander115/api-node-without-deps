20b9a67f5911ab2936979bf4b89d5cc4295fd590
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-02-04 21:57:35 -0300

    feat: add local database with database class

--- src/server.js
+++ src/server.js
@@ -1,38 +1,33 @@
+import { randomUUID } from 'node:crypto';
 import http from 'node:http';
+import { Database } from './database.js';
+import { json } from './middlewares/json.js';
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    request.body = null;
-  }
+  await json(request, response);
 
   console.log(request.body);
 
   if (method === 'GET' && url === '/users') {
-    return response
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users));
+    const users = database.select('users');
+
+    return response.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = request.body;
 
-    users.push({
-      id: 'sodjfoidjfosdfsifhishdf',
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    });
+    };
+
+    database.insert('users', user);
 
     return response.writeHead(201).end();
   }
