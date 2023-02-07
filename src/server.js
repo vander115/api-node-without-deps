23cdfaa3a4e8cb6c481c4ef09c589b34880205a9
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-02-06 21:28:02 -0300

    feat: add delete route with regex to get id of route

--- src/server.js
+++ src/server.js
@@ -5,13 +5,19 @@ import { routes } from './routes.js';
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
+  // middlewares interceptam as requisições e fazem interações com os dados antes de prosseguir com a função.
   await json(request, response);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+
+    const routeParams = request.url.match(route.path)
+
+    request.params = { ...routeParams.groups }
+
     return route.handler(request, response);
   }
 
